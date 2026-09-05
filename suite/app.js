b40dd5e659873616645998832baa9280643d8e4c
Sajeevan Veeriah <sajeevanveeriah@gmail.com>
2026-09-05 20:21:12 +1000

    Synchronise verified operating-control release with static Pages fallback

--- suite/app.js
+++ suite/app.js
@@ -1,1 +1,1 @@
-location.replace(new URL('../?release=potato-only-4#plant',document.currentScript?.src || document.baseURI).href);
+location.replace(new URL('../?release=potato-controls-5#plant',document.currentScript?.src || document.baseURI).href);
