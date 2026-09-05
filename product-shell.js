77513c71f65271879998e918c1c092d96bdc4126
Sajeevan Veeriah <sajeevanveeriah@gmail.com>
2026-09-05 19:58:21 +1000

    Synchronise potato-only release with cacheless page migration

--- product-shell.js
+++ product-shell.js
@@ -1,1 +1,1 @@
-location.replace(new URL('./?release=potato-only-3#plant',document.currentScript?.src || document.baseURI).href);
+location.replace(new URL('./?release=potato-only-4#plant',document.currentScript?.src || document.baseURI).href);
